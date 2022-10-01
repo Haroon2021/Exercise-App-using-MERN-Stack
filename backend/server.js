5bb7bbd98e5a43155bc474d557d45d045094ea3e
haroon <haroon@Dures-MacBook-Air.local>
2022-10-01 15:48:25 +0100

    Main routes setup

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,15 @@ require('dotenv').config()
 // require express package
 
 const express =require('express')
+const workoutRoutes = require('/routes/workouts')
 
 // express app crated
 const app = express()
 
 //MIDDLEWARE
+//the line below allows you to dealwith data being sent form the client eg from a post or update request
+app.use(express.json())
+
 app.use((req,res,next) => {
     console.log(req.path, req.method)
     next()
@@ -15,9 +19,7 @@ app.use((req,res,next) => {
 
 
 //routes
-app.get('/', (req, res) => {
-    res.json({mssg: "Welcome to the app"})
-})
+app.use('/api/workouts',workoutRoutes)
 
 // Listen for requests
 app.listen(process.env.PORT, () =>{
