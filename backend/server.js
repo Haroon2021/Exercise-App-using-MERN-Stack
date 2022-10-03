461a9da5cc40dbd9165a16fdb47276b0e04a4bdf
haroon <haroon@Dures-MacBook-Air.local>
2022-10-03 21:01:26 +0100

    connected to mongo db

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,14 @@ require('dotenv').config()
 // require express package
 
 const express =require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
 // express app crated
 const app = express()
 
 //MIDDLEWARE
-//the line below allows you to dealwith data being sent form the client eg from a post or update request
+//the line below allows you to deal with data being sent form the client eg from a post or update request
 app.use(express.json())
 
 app.use((req,res,next) => {
@@ -21,9 +22,18 @@ app.use((req,res,next) => {
 //routes
 app.use('/api/workouts',workoutRoutes)
 
-// Listen for requests
-app.listen(process.env.PORT, () =>{
-    console.log('listenting on port',process.env.PORT)
-})
+// connect to database
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=> {
+        // Listen for requests
+    app.listen(process.env.PORT, () =>{
+    console.log('connected to db and listenting on port',process.env.PORT)
+    })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
+
 
 
